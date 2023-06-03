2099a214d95f0bf5e3d4c15f10913b08538bfb11
Aidan Fournier <aidan.douglas.fournier@gmail.com>
2023-06-03 13:58:07 +0900

    Fixed bugs with Tailwind and Navigator

---
+++ screens/StatsScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const StatsScreen = () => {
+  return (
+    <View>
+      <Text>StatsScreen</Text>
+    </View>
+  )
+}
+
+export default StatsScreen;
