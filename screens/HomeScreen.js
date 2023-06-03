b30058cf3955c2b8e5d62850ff54898c9b5ca31e
Miriam Fiandanese <mmiy55@gmail.com>
2023-06-03 23:13:05 +0900

    Revert "delete proxy after CORS fix"
    This reverts commit 220ddfc6eceb0abfc5479ebbccefd17f75fe45bc.
    Revert for commit on main by mistake

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -4,9 +4,10 @@ import React,{useState, useEffect} from 'react';
 const HomeScreen = () => {
 
 const getConsumptionFromApi = () => {
+    const corsAnywhereUrl = 'https://cors-anywhere.herokuapp.com/';
     const apiUrl = 'https://kraken-japan-hackathon-api.onrender.com/products/AGILE-23-06-01/product_rates/consumption_charges';
 
-    return fetch(apiUrl)
+    return fetch(corsAnywhereUrl + apiUrl)
       .then(response => response.json())
       .then(json => {
         return json;
