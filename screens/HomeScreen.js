ef44fa26e92316d8bc3418db8401a74cdc72cb98
Miriam Fiandanese <mmiy55@gmail.com>
2023-06-04 15:35:05 +0900

    delete api logic from home

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -24,47 +24,6 @@ const styles = StyleSheet.create({
 
 const HomeScreen = () => {
 
-  // product rate fetch
-  // return the price (in yen) of kWh (kilowatt-hour)/ 30 minutes slot for the next 10 days
-  const getProductRateFromApi = () => {
-    const productCode = 'AGILE-23-06-01';
-    const rateType = 'consumption_charges';
-    const apiUrl = `${baseUrl}/products/${productCode}/product_rates/${rateType}`;
-
-    return fetch(apiUrl)
-      .then(response => response.json())
-      .then(json => {
-        return json;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
-  getProductRateFromApi().then(data => console.log(data));
-
-  // consumption fetch
-  // return how much kWh used by the consumer in a given period of time
-  const getConsumptionFromApi = () => {
-    // spin is required and identifies the user
-    const spin = '0300111001601606060001';
-    // period is optional but if not specified return all the pages
-    const periodFrom = '2023-06-02T06:00:00+09:00';
-    const periodTo = '2023-06-02T07:00:00+09:00';
-
-    const apiUrl = `${baseUrl}/supply_points/${spin}/consumption?period_from=${encodeURIComponent(periodFrom)}&period_to=${encodeURIComponent(periodTo)}`;
-
-    return fetch(apiUrl)
-      .then(response => response.json())
-      .then(json => {
-        return json;
-      })
-      .catch(error => {
-        console.error(error);
-      });
-  };
-
-
   return (
     <View className=" flex item-center bg-slate-900 h-full">
       <View className=" mx-60 mt-8">
